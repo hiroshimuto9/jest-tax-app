09a778b01a51e69fd52494cb090f3b37999cd9e8
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-08 23:20:02 +0900

    テストが成功した状態を維持したまま、テストコードをリファクタリング

--- tax-api/src/calcTax.test.ts
+++ tax-api/src/calcTax.test.ts
@@ -1,27 +1,79 @@
 import { calcRetirementIncomeDeduction } from './calcTax'
 
-describe('退職所得控除', () => {
-  test.each`
-    yearsOfService | isDisability | expected
-    ${1}|${false}|${800_000}
-    ${2}|${false}|${800_000}
-    ${3}|${false}|${1_200_000}
-    ${19}|${false}|${7_600_000}
-    ${20}|${false}|${8_000_000}
-    ${21}|${false}|${8_700_000}
-    ${30}|${false}|${15_000_000}
-    ${1}|${true}|${1_800_000}
-    ${2}|${true}|${1_800_000}
-    ${3}|${true}|${2_200_000}
-    ${19}|${true}|${8_600_000}
-    ${20}|${true}|${9_000_000}
-    ${21}|${true}|${9_700_000}
-    ${30}|${true}|${16_000_000}
-  `('yearsOfService年 isDisability:$isDisability→$expected', ({ yearsOfService, isDisability, expected }) => {
+describe('退職所得控除額', () => {
+  const testDeduction = (yearsOfService: number, isDisability: boolean, expected: number) => {
     const deduction = calcRetirementIncomeDeduction({
       yearsOfService,
       isDisability,
     })
     expect(deduction).toBe(expected);
+  };
+
+  describe('勤続年数が1年の場合', () => {
+    describe('障害者となったことに直接基因して退職」に該当しない場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${1}|${800_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, false, expected)
+      })
+    })
+
+    describe('障害者となったことに直接基因して退職」に該当する場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${1}|${1_800_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, true, expected)
+      })
+    })
+  })
+
+  describe('勤続年数が2年以上20年未満の場合', () => {
+    describe('障害者となったことに直接基因して退職」に該当しない場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${2}|${800_000}
+        ${3}|${1_200_000}
+        ${19}|${7_600_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, false, expected)
+      })
+    })
+
+    describe('障害者となったことに直接基因して退職」に該当する場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${2}|${1_800_000}
+        ${3}|${2_200_000}
+        ${19}|${8_600_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, true, expected)
+      })
+    })
+  })
+
+  describe('勤続年数が20年以上の場合', () => {
+    describe('障害者となったことに直接基因して退職」に該当しない場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${20}|${8_000_000}
+        ${21}|${8_700_000}
+        ${30}|${15_000_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, false, expected)
+      })
+    })
+
+    describe('障害者となったことに直接基因して退職」に該当する場合', () => {
+      test.each`
+        yearsOfService | expected
+        ${20}|${9_000_000}
+        ${21}|${9_700_000}
+        ${30}|${16_000_000}
+      `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
+        testDeduction(yearsOfService, true, expected)
+      })
+    })
   })
 })
