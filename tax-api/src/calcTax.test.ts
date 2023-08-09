cbb20b6a5e23e320cb463fc0c448dd0742f31563
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-09 14:11:29 +0900

    課税退職所得金額のテストコードを実装

--- tax-api/src/calcTax.test.ts
+++ tax-api/src/calcTax.test.ts
@@ -1,4 +1,4 @@
-import { calcRetirementIncomeDeduction } from './calcTax'
+import { calcRetirementIncomeDeduction, calcTaxableRetirementIncome } from './calcTax'
 
 describe('退職所得控除額', () => {
   const testDeduction = (yearsOfService: number, isDisability: boolean, expected: number) => {
@@ -74,6 +74,103 @@ describe('退職所得控除額', () => {
       `('勤続年数$yearsOfService年→$expected円', ({ yearsOfService, expected }) => {
         testDeduction(yearsOfService, true, expected)
       })
+    })
+  })
+})
+
+describe('課税退職所得金額', () => {
+  // 勤続年数が 6 年以上の場合
+  describe('勤続年数が6年以上の場合', () => {
+    test.each`
+      yearsOfService | severancePay | deduction | isBoardMember | expected
+      ${6} | ${3_000_000} | ${2_400_000} | ${false} | ${300_000}
+      ${6} | ${3_000_000} | ${2_400_000} | ${true}   | ${300_000}
+      ${6} | ${3_001_999} | ${2_400_000} | ${false}  | ${300_000}
+      ${6} | ${3_001_999} | ${2_400_000} | ${true}   | ${300_000}
+      ${6} | ${3_002_000} | ${2_400_000} | ${false}  | ${301_000}
+      ${6} | ${3_002_000} | ${2_400_000} | ${true}   | ${301_000}
+      ${6} | ${1_000_000} | ${2_400_000} | ${false}  | ${0}
+      ${6} | ${1_000_000} | ${2_400_000} | ${true}   | ${0}
+    `(
+      '勤続年数$yearsOfService年・退職金$severancePay円・退職所得控除額$deduction円・' +
+      '役員等$isOfficer → $expected円',
+      ({ yearsOfService, severancePay, deduction, isBoardMember, expected }) => {
+        const taxableIncome = calcTaxableRetirementIncome({
+          yearsOfService,
+          severancePay,
+          isBoardMember,
+          retirementIncomeDeduction: deduction,
+        })
+        expect(taxableIncome).toBe(expected)
+      },
+    )
+  })
+
+  describe('役員等で勤続年数が5年以下の場合', () => {
+    test.each`
+      yearsOfService | severancePay | deduction    | expected
+      ${5}           | ${3_000_000} | ${2_000_000} | ${1_000_000}
+      ${5}           | ${3_000_999} | ${2_000_000} | ${1_000_000}
+      ${5}           | ${3_001_000} | ${2_000_000} | ${1_001_000}
+      ${5}           | ${1_000_000} | ${2_000_000} | ${0}
+    `(
+      '勤続年数$yearsOfService年・退職金$severancePay円・退職所得控除額$deduction円 → ' +
+      '$expected円',
+      ({ yearsOfService, severancePay, deduction, expected }) => {
+        const taxableIncome = calcTaxableRetirementIncome({
+          yearsOfService,
+          severancePay,
+          isBoardMember: true,
+          retirementIncomeDeduction: deduction,
+        })
+        expect(taxableIncome).toBe(expected)
+      },
+    )
+  })
+
+  describe('役員等以外で勤続年数が5年以下の場合', () => {
+    describe('控除後の金額が300万円以下の場合', () => {
+      test.each`
+        yearsOfService | severancePay | deduction    | expected
+        ${5}           | ${3_000_000} | ${2_000_000} | ${500_000}
+        ${5}           | ${5_000_000} | ${2_000_000} | ${1_500_000}
+        ${5}           | ${3_001_999} | ${2_000_000} | ${500_000}
+        ${5}           | ${3_002_000} | ${2_000_000} | ${501_000}
+        ${5}           | ${1_000_000} | ${2_000_000} | ${0}
+      `(
+        '勤続年数$yearsOfService年・退職金$severancePay円・退職所得控除額$deduction円 → ' +
+        '$expected円',
+        ({ yearsOfService, severancePay, deduction, expected }) => {
+          const taxableIncome = calcTaxableRetirementIncome({
+            yearsOfService,
+            severancePay,
+            isBoardMember: false,
+            retirementIncomeDeduction: deduction,
+          })
+          expect(taxableIncome).toBe(expected)
+        },
+      )
+    })
+
+    describe('控除後の金額が300万円を超える場合', () => {
+      test.each`
+        yearsOfService | severancePay | deduction    | expected
+        ${5}           | ${6_000_000} | ${2_000_000} | ${2_500_000}
+        ${5}           | ${6_001_999} | ${2_000_000} | ${2_501_000}
+        ${5}           | ${6_002_000} | ${2_000_000} | ${2_502_000}
+      `(
+        '勤続年数$yearsOfService年・退職金$severancePay円・退職所得控除額$deduction円 → ' +
+        '$expected円',
+        ({ yearsOfService, severancePay, deduction, expected }) => {
+          const taxableIncome = calcTaxableRetirementIncome({
+            yearsOfService,
+            severancePay,
+            isBoardMember: false,
+            retirementIncomeDeduction: deduction,
+          })
+          expect(taxableIncome).toBe(expected)
+        },
+      )
     })
   })
 })
