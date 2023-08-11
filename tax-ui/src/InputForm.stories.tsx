39793860472e1106b9762120c00dd7e1dac54cc2
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-11 17:44:42 +0900

    feat: storybookの環境を構築

---
+++ tax-ui/src/InputForm.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { InputForm } from './InputForm'
+
+export default {
+  component: InputForm,
+} as Meta<typeof InputForm>
+
+export const Standard: StoryObj<typeof InputForm> = {}
---
+++ tax-ui/src/InputForm.tsx
@@ -0,0 +1,3 @@
+import { Button } from '@chakra-ui/react'
+
+export const InputForm = () => <Button colorScheme="blue">Hello</Button>
