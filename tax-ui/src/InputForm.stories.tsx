92d11018607e9f8324e74495fd02f5b32c184a78
Hiroshi Muto <mhi6o461o@gmail.com>
2023-08-13 23:34:38 +0900

    delete: 不要ストーリーの削除

--- tax-ui/src/InputForm.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-
-import { InputForm } from './InputForm'
-
-export default {
-  component: InputForm,
-} as Meta<typeof InputForm>
-
-export const Standard: StoryObj<typeof InputForm> = {}
